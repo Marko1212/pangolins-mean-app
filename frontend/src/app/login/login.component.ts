08b16a6a02926b16885f45e61781cf5964751bd6
Maxime Petit <maxime.petit@epitech.eu>
2020-12-15 17:44:18 +0100

    Login

--- frontend/src/app/login/login.component.ts
+++ frontend/src/app/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -7,10 +10,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  loginFrom: FormGroup;
+  errorMessage: string;
+
+  constructor(private formBuilder: FormBuilder,
+              private router: Router,
+              private auth: AuthService) { }
 
   ngOnInit(): void {
+    this.loginForm = this.formBuilder.group({
+      email: [null, [Validators.required, Validators.email]],
+      password: [null, Validators.required]
+    });
   }
 
-  onSubmit() { }
+  onSubmit() {
+    const email = this.loginForm.get('email').value;
+    const password = this.loginForm.get('password').value;
+    this.auth.login(email, password).then(
+      () => {
+        this.router.navigate(['/']);
+      }
+    ).catch(
+      (error) => {
+        this.errorMessage = error.message;
+      }
+    );
+  }
 }
