860c26f4e6db8ee5f4a57295b8e984e26ac9baa8
Maxime Petit <maxime.petit@epitech.eu>
2020-12-15 17:37:56 +0100

    Signup

--- frontend/src/app/signup/signup.component.ts
+++ frontend/src/app/signup/signup.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-signup',
@@ -7,10 +10,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignupComponent implements OnInit {
 
-  constructor() { }
+  signupForm: FormGroup;
+  errorMessage: string;
+
+  constructor(private formBuilder: FormBuilder,
+    private router: Router,
+    private auth: AuthService) { }
 
   ngOnInit(): void {
+    this.signupForm = this.formBuilder.group({
+      email: [null, [Validators.required, Validators.email]],
+      password: [null, Validators.required],
+      age: [null, [Validators.min(1), Validators.max(20), Validators.pattern('^[0-9]\d*$')]],
+      family: [null],
+      breed: [null],
+      food: [null]
+    });
   }
 
-  onSubmit() { }
+  onSignup() {
+    const email = this.signupForm.get('email').value;
+    const password = this.signupForm.get('password').value;
+    const age = this.signupForm.get('age').value;
+    const family = this.signupForm.get('family').value;
+    const breed = this.signupForm.get('breed').value;
+    const food = this.signupForm.get('food').value;
+    this.auth.createNewUser(email, password, age, family, breed, food).then(
+      () => {
+        this.router.navigate(['/']);
+      }
+    ).catch(
+      (error) => {
+        this.errorMessage = error.message;
+      }
+    );
+  }
 }
