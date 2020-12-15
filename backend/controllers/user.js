5d0a50519bfa09edd71634e734cf5a428d3c8f84
Maxime Petit <maxime.petit@epitech.eu>
2020-12-15 16:12:56 +0100

    Login post route

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,4 +1,5 @@
 const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
 
 const User = require('../models/User')
 
@@ -17,6 +18,31 @@ exports.signup = (req, res, next) => {
       user.save()
         .then(() => res.status(201).json({ message: 'User saved !' }))
         .catch(error => res.status(400).json({ error }))
+    })
+    .catch(error => res.status(500).json({ error }))
+}
+
+exports.login = (req, res, next) => {
+  User.findOne({ email: req.body.email })
+    .then(user => {
+      if (!user) {
+        return res.status(401).json({ error: 'Unknown email or password' })
+      }
+      bcrypt.compare(req.body.password, user.password)
+        .then(valid => {
+          if (!valid) {
+            return res.status(401).json({ error: 'Unknown email or password' })
+          }
+          res.status(200).json({
+            userId: user._id,
+            token: jwt.sign(
+              { userId: user._id },
+              'DLM6q8Ld0J8rrSK41LmG',
+              { expiresIn: '24h' }
+            )
+          })
+        })
+        .catch(error => res.status(500).json({ error }))
     })
     .catch(error => res.status(500).json({ error }))
 }
