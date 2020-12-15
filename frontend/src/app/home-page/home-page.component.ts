fa68c6de514dd7d15d1bbaab4b4281a3d5f17b4b
Maxime Petit <maxime.petit@epitech.eu>
2020-12-15 23:23:39 +0100

    home-page: friends display

--- frontend/src/app/home-page/home-page.component.ts
+++ frontend/src/app/home-page/home-page.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../services/auth.service';
+import { UserService } from '../services/user.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home-page',
@@ -7,9 +10,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomePageComponent implements OnInit {
 
-  constructor() { }
+  friends: string[] = [];
+  users: string[] = [];
+
+  constructor(private auth: AuthService,
+              private userService: UserService,
+              private router: Router) { }
 
   ngOnInit(): void {
+    this.userService.getFriendsByEmail(this.auth.userEmail).then(friends => {
+      friends.friends.forEach(id => {
+        this.userService.getUserById(id).then(friend => {
+          this.friends.push(friend);
+        })
+        .catch((error) => { console.log(error) });
+      });
+    })
+    .catch((error) => { console.log(error) });
+
+    this.userService.getUsers().then(users => {
+      this.users = users;
+    })
+    .catch((error) => { console.log(error) });
   }
 
+  onUserClick(user: string) {
+    if (user._id == this.auth.userId) return;
+    this.userService.addFriend(this.auth.userEmail, user._id).then(() => {
+      console.log('New friend');
+    })
+    .catch((error) => { console.log(error) });
+  }
 }
