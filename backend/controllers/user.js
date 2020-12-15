36966919928deabd139677a925a90b1e9794c895
Maxime Petit <maxime.petit@epitech.eu>
2020-12-15 18:25:16 +0100

    get one user route

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -46,3 +46,9 @@ exports.login = (req, res, next) => {
     })
     .catch(error => res.status(500).json({ error }))
 }
+
+exports.getOneUser = (req, res, next) => {
+  User.findOne({ _id: req.params.id })
+    .then(user => res.status(200).json(user))
+    .catch(error => res.status(404).json({ error }))
+}
